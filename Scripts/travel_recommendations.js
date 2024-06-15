9eb50df8c3d1dc25c47edf51c679e168a7f390d8
BexS673 <rsayer532@gmail.com>
2024-06-15 12:40:39 +0100

    Fixed problems with recommendations and styling

--- Scripts/travel_recommendations.js
+++ Scripts/travel_recommendations.js
@@ -1,12 +1,20 @@
-async function searchDestination(){
+/**
+ * Search function.
+ */
+function searchDestination() {
 
-    // wait for home html to load in
-    let homeLoaded = await loadHome();
+    let homeLoaded = loadHome();
     var destinationSearch = document.getElementById("dest_search").value.toLowerCase();
-    produceRecommendations(destinationSearch);    
+    produceRecommendations(destinationSearch); 
 }
 
-function produceRecommendations(search){
+/**
+ * This function updates the page to show the recommendations based on the user's search.
+ * @param {String} search seach value
+ */
+function produceRecommendations(search) {
+
+    // make this async function?????
     fetch("../travel_recommendations.json")
     .then(response => {
         return response.json();
@@ -27,96 +35,108 @@ function produceRecommendations(search){
             contentDiv.innerHTML = "";
 
             // add to the html each of the recommendations from the returned results
-            destinationResults.forEach(destination => {
-                
-                //let destinationTime = ct.getTimeZone(destination.name);
-                //console.log(destinationTime);//displayTime(destination.name);
-                destinationTime = "14:00";
-                let titleConfig = getTitle(destination.name);
-                
-                contentDiv.innerHTML += getHTML(destination);                                     
-
-                                        
-                                    });
+            destinationResults.forEach(destination => {                                
+                // 
+                getHTML(search, destination);    
+            });
         }
     })
     .catch(error => "Cannot access JSON");
 
 }
 
-function getCountryData(){
-    //FIX!!!!!!!!!!!!!!!!!!!!!!!!!!
-}
+/**
+ * Insert the HTML into page for each destination of the search result
+ * @param {String} searchResult search value
+ * @param {any} destination element in array of search object
+ */
+function getHTML(searchResult, destination) {
 
-function getHTML(destination){
-    let htmlString =`<h2 class="accordion-header recommendation">
-                                        <button class="accordion-button rounded collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#${destination.id}" aria-expanded="false" aria-controls=${destination.id}>
-                                            <h3>
-                                              ${titleConfig[0]}
-                                              <small class="text-body-secondary">${titleConfig[1]}</small>
-                                            </h3>
-                                        </button>
-                                        </h2>
-                                        <div id=${destination.id} class="accordion-collapse collapse" data-bs-parent="#recommend_list">
-                                            <div class="accordion-body">
-                                                <img class="imageUrl rounded" src="${destination.imageUrl}">
-                                                ${destination.description} <br />
-                                                <em>Local Time: ${destinationTime}</em>
-                                            </div>
-                                        </div>`
-    return htmlString;
-}
-
-function getTitle(name){
-    if (name.includes(",")){
-        return name.split(",");
-    } else {
-        return [name, ""];
+    function getTitle(name) {
+        if (name.includes(",")) {
+            return name.split(",");
+        } else {
+            return [name, ""];
+        }
     }
+
+    let titleConfig = getTitle(destination.name);
+    destinationTime = "14:00";
+
+    // HTML accordion item and add to the recommend list
+    let htmlString = `<h2 class="accordion-header recommendation">
+                        <button class="accordion-button rounded collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#${destination.id}" aria-expanded="false" aria-controls=${destination.id}>
+                            <h3>
+                                ${titleConfig[0]}
+                                <small class="text-body-secondary">${titleConfig[1]}</small>
+                            </h3>
+                        </button>
+                      </h2>
+                     <div id="${destination.id}" class="accordion-collapse collapse" data-bs-parent="#recommend_list">
+                        <div class="accordion-body" id="${destination.id}_body">
+                        </div>
+                     </div>`;
+
+    let contentDiv = document.getElementById("recommend_list");
+    contentDiv.innerHTML += htmlString;
+
+    // Get the HTML for the accordion item's body, depending on the search type
+    let htmlBody = "";
+    var bodyElement = document.getElementById(`${destination.id}_body`);
+
+    if (searchResult != "countries") {
+        htmlBody += `<div class="card">
+                      <img src="${destination.imageUrl}" class="card-img-top" alt="">
+                      <div class="card-body">
+                        <p class="card-text">${destination.description}</p>
+                      </div>
+                    </div>`;
+        bodyElement.innerHTML = htmlBody;
+                        
+    } else {   
+
+        bodyElement.innerHTML = `<ul id="${destination.id}_card_list"></ul>`;
+        var cardList = document.getElementById(`${destination.id}_card_list`);
+
+        destination.cities.forEach(city => {
+            
+            htmlBody = `<li><div class="card" style="width: 18rem;">
+                          <img src="${city.imageUrl}" class="card-img-top" alt="...">
+                          <div class="card-body">
+                          <h5 class="card-title">${city.name}</h5>
+                            <p class="card-text">${city.description}</p>
+                          </div>
+                        </div></li>`;             
+            cardList.innerHTML += htmlBody;
+        });        
+    }
+        
 }
 
-function getLocalTime(){}
-
-function displayTime(countryName){
-    const options = { timeZone: countryName, hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' };
-    const countryTime = new Date().toLocaleTimeString('en-US', options);
-    console.log(`Current time in ${countryName}:`, countryTime);
-}
-
-function clearHighlighted(){
-
-    var keywords = document.querySelectorAll(".keyword");
-    keywords.forEach(keyword => {
-        keyword.style["font-weight"]="";
-    });
-}
-
+/**
+ * Clear the recommendations from the page.
+ */
 function clearSearch(){
     // Clear recommendations
+
+    // FIX: when cards are open, the bars close first and leave the cards...
     let recommendations = document.getElementById("recommendations");
     recommendations.style.visibility="hidden";
-
-    clearHighlighted();
 }
 
-function togglePopup(popupId){
-    var popup = document.getElementById(popupId);
-    if (popup.style.visibility == 'visible'){
-        popup.style.visibility = 'hidden';
-    } else {
-        popup.style.visibility = 'visible';
-    }
-}
-
+/**
+ * 
+ * @param {String} pageURL HTML page to fetch HTML content from to update the main content of the page
+ */
 async function loadContent(pageURL){
-    let getContnet = await fetch(pageURL)
+    let getContent = await fetch(pageURL)
     .then(response => response.text())
     .then(text => {
             let contentDiv = document.getElementById("main_content");
                 contentDiv.innerHTML = text;
         });
 
-    // re-initialise any popovers in the html
+    // re-initialise any popovers in the updated HTML
     const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
     const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 }
@@ -125,18 +145,21 @@ function loadHome(){
     loadContent("../Templates/home.html");
 }
 
-// event listeners for navigation tab
+
+//****************************** EVENT LISTENERS ****************************************/
+
+/**
+ * Event listeners for updating the page content from the navigation page.
+ */
 $(document).on("click", "#home", function(){loadContent("../Templates/home.html")});
 $(document).on("click", "#about_us", function(){loadContent("../Templates/about_us.html")});
 $(document).on("click", "#contact_us", function(){loadContent("../Templates/contact_us.html")});
 
-
-// event listeners for recommendations via homepage
-
-// beach
+/**
+ * Event listeners for user interaction with the beach search
+ */
 $(document).on("click", "#beach", function(){
     produceRecommendations("beaches");
-    clearHighlighted();
     document.getElementById("beach").style["font-weight"] = "bolder";
     });
 
@@ -150,11 +173,11 @@ $(document).on("mouseout", "#beach", function(){
     popover.hide();   
 });
 
-// temples
-
+/**
+ * Event listeners for user interaction with the temple search
+ */
 $(document).on("click", "#culture", function(){
     produceRecommendations("temples");
-    clearHighlighted();
     document.getElementById("culture").style["font-weight"] = "bolder";
     });
 
@@ -168,11 +191,11 @@ $(document).on("mouseout", "#culture", function(){
     popover.hide();
 });
 
-// cities
-
+/**
+ * Event listeners for user interaction with the country search
+ */
 $(document).on("click", "#country", function(){
     produceRecommendations("countries");
-    clearHighlighted();
     document.getElementById("country").style["font-weight"] = "bolder";
     });
 
@@ -193,4 +216,15 @@ $(document).on("mouseout", "#country", function(){
     //The search button isn't working anymore!'
     //appears then disappears....
 
-    //countries also only has a description for cities so another depth in
+
+
+
+
+function togglePopup(popupId) {
+    var popup = document.getElementById(popupId);
+    if (popup.style.visibility == 'visible') {
+        popup.style.visibility = 'hidden';
+    } else {
+        popup.style.visibility = 'visible';
+    }
+}
